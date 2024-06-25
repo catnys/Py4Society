b26e227441a6a339e4b016163bf7cb6ba5593dc5
catnys <catnyseverdyn@gmail.com>
2024-06-25 13:12:23 +0300

    Day008: init part3 caesar

--- Day008-Beginner/Caesar-Cyper/part3.py
+++ Day008-Beginner/Caesar-Cyper/part3.py
@@ -6,25 +6,40 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
+def encrypt(text, shift):
+    cipher_text = ""
+    for char in text:
+      index = int(alphabet.index(char))
+      newIndex = (index + shift) % int(len(alphabet))
+      cipher_text += alphabet[newIndex]
+    print(cipher_text)
+    return cipher_text
 
-def decrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The decoded text is {plain_text}")
 
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_amount=shift)
+# TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(text, shift):
+    decrypt_text = ""
+
+    for letter in text:
+      index = int(alphabet.index(letter))
+      newIndex = (index - shift) % len(alphabet)
+      decrypt_text += alphabet[newIndex]
+
+    print(decrypt_text)
+    return decrypt_text
+
+
+def main():
+    if direction == "encode":
+      encrypted_text = encrypt(text, shift)
+    elif direction == "decode":
+      decrypted_text = decrypt(text, shift)
+
+
 
 #TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+
+
+
+if __name__ == "__main__":
+  main()
