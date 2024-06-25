3113b58fbd73480917844c5befca555d5d05f192
catnys <catnyseverdyn@gmail.com>
2024-06-25 12:52:08 +0300

    Day008: update part 2

--- Day008-Beginner/Caesar-Cyper/part2.py
+++ Day008-Beginner/Caesar-Cyper/part2.py
@@ -1,21 +1,23 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+direction = 'decrypt' #input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = 'civilization' # input("Type your message:\n").lower()
+shift = 5 # int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
+def encrypt(text, shift):
+    cipher_text = ""
+    for char in text:
+        index = int(alphabet.index(char))
+        newIndex = (index + shift) % int(len(alphabet))
+        cipher_text += alphabet[newIndex]
+    print(cipher_text)
+    return cipher_text
+
 
 #TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
-  #e.g. 
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+  #e.g.
   #cipher_text = "mjqqt"
   #shift = 5
   #plain_text = "hello"
@@ -23,4 +25,13 @@ def encrypt(plain_text, shift_amount):
 
 
 #TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-encrypt(plain_text=text, shift_amount=shift)
+
+
+def main():
+    encrypt(text,shift)
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+
+
+if __name__ == "__main__":
+    main()
