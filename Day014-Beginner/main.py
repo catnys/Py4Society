f707bcede1462adee0f793dae4d93de93800ccaf
catnys <catnyseverdyn@gmail.com>
2024-06-27 14:54:11 +0300

    Day014: update main structure

--- Day014-Beginner/main.py
+++ Day014-Beginner/main.py
@@ -1,8 +1,27 @@
 import random
 from art import logo
+from gamedata import data
+
+
+
+def displayGameData(list):
+    for item in list:
+        print(item)
+
+
+def displayLogo():
+    """Displays the logo."""
+    print(logo)
+
 
 
 def main():
-    print(logo)
+
+
+
+
+
+
+
 if __name__ == '__main__':
     main()
