77ca54fa30df03488ce235ea34e13a52d8db3669
catnys <catnyseverdyn@gmail.com>
2024-06-30 16:12:12 +0300

    Day020: add snake class

--- Day020-Intermediate/snake.py
+++ Day020-Intermediate/snake.py
@@ -1,2 +1,22 @@
 class Snake:
-    def __init__(self):
+    # add constructor
+    def __init__(self, health=1, length=3):
+        self.health = health
+        self.length = length
+
+    # getters and setters
+
+    def getHealth(self):
+        return self.health
+
+    def getLength(self):
+        return self.length
+
+    def setHealth(self, health):
+        self.health = health
+
+    def setLength(self, length):
+        self.length = length
+
+    def __str__(self):
+        return f"{self.length},{self.health}"
