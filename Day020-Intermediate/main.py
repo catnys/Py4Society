558889c22fd90ca499eb74f4c17a43db69433f62
catnys <catnyseverdyn@gmail.com>
2024-06-30 18:55:30 +0300

    Day020: update main

--- Day020-Intermediate/main.py
+++ Day020-Intermediate/main.py
@@ -2,27 +2,31 @@ from turtle import Screen
 from snake import Snake
 import time
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
-screen.tracer(0)
+def main():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("My Snake Game")
+    screen.tracer(0)
 
-snake = Snake()
-food = Food()
+    snake = Snake()
+    food = Food()
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
+    isGameOn = True
+    while isGameOn:
+        screen.update()
+        time.sleep(0.1)
 
-    snake.move()
+        snake.move()
 
 
-screen.exitonclick()
+    screen.exitonclick()
+
+if __name__ == '__main__':
+    main()
