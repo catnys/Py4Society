4e2b5f8c881bacfc56f3bfc8f37ce4989e316e63
catnys <catnyseverdyn@gmail.com>
2024-07-02 10:47:09 +0300

    ADd moveUp or Down methods

--- Day022-Intermediate/Pong/main.py
+++ Day022-Intermediate/Pong/main.py
@@ -7,13 +7,34 @@ def setScreen(screen: Screen):
     screen.bgcolor('black')
     screen.setup(800, 600)
     screen.title("Pong")
-    screen.exitonclick()
+
+
+def moveUp(paddle):
+    newY = paddle.ycor() + 20
+    paddle.goto(paddle.xcor, newY)
+
+
+def moveDown(paddle):
+    newY = paddle.ycor() - 20
+    paddle.goto(paddle.xcor, newY)
 
 
 def main():
     """"main function"""
     screen = Screen()
     setScreen(screen)
+
+    paddle = Turtle()
+    paddle.shape('square')
+    paddle.color('white')
+    paddle.shapesize(stretch_wid=5, stretch_len=1)
+    paddle.penup()
+    paddle.goto(350, 0)
+
+    screen.listen()
+    screen.onkey(moveUp, "Up")
+
+    screen.exitonclick()
 
 
 if __name__ == "__main__":
