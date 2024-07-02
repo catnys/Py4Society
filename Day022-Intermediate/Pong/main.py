625854ece6cb9434bc73052929b0f134790a876c
catnys <catnyseverdyn@gmail.com>
2024-07-02 10:35:59 +0300

    Day022: add setScreen method

--- Day022-Intermediate/Pong/main.py
+++ Day022-Intermediate/Pong/main.py
@@ -1,6 +1,19 @@
+from turtle import Turtle, Screen
+
+
+def setScreen(screen: Screen):
+    """Adjust the screen of the pong game"""
+    screen.listen()
+    screen.bgcolor('black')
+    screen.setup(800, 600)
+    screen.title("Pong")
+    screen.exitonclick()
+
+
 def main():
     """"main function"""
-
+    screen = Screen()
+    setScreen(screen)
 
 
 if __name__ == "__main__":
