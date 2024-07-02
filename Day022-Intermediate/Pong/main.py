cbfde1102e9caa12b913cee3badb33afe42e94e8
catnys <catnyseverdyn@gmail.com>
2024-07-02 11:20:43 +0300

    Day022: update main methods

--- Day022-Intermediate/Pong/main.py
+++ Day022-Intermediate/Pong/main.py
@@ -3,24 +3,32 @@ from turtle import Turtle, Screen
 
 def setScreen(screen: Screen):
     """Adjust the screen of the pong game"""
-    screen.listen()
     screen.bgcolor('black')
     screen.setup(800, 600)
     screen.title("Pong")
+    screen.tracer(0)
 
 
-def moveUp(paddle):
+def refreshScreen(screen: Screen):
+    """Refresh the screen of the pong game"""
+    screen.update()
+
+
+def moveUp(paddle) -> None:
     newY = paddle.ycor() + 20
     paddle.goto(paddle.xcor, newY)
 
 
-def moveDown(paddle):
+def moveDown(paddle) -> None:
     newY = paddle.ycor() - 20
     paddle.goto(paddle.xcor, newY)
 
 
 def main():
     """"main function"""
+
+    isGameOn = True
+
     screen = Screen()
     setScreen(screen)
 
@@ -30,9 +38,12 @@ def main():
     paddle.shapesize(stretch_wid=5, stretch_len=1)
     paddle.penup()
     paddle.goto(350, 0)
-
     screen.listen()
-    screen.onkey(moveUp, "Up")
+    screen.onkey(lambda: moveUp(paddle), "Up")
+    screen.onkey(lambda: moveDown(paddle), "Down")
+
+    while isGameOn:
+        screen.update()
 
     screen.exitonclick()
 
