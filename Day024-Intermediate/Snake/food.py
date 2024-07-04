35a072769a1a1fd09a719ae594550d719e17ebee
catnys <catnyseverdyn@gmail.com>
2024-07-04 22:25:02 +0300

    Day024: add intermediate 24 as init

---
+++ Day024-Intermediate/Snake/food.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.penup()
+        self.shape("circle")
+        self.color("red")
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        randomX = random.randint(-280, 280)
+        randomY = random.randint(-280, 280)
+        self.goto(randomX, randomY)
---
+++ Day024-Intermediate/Snake/main.py
@@ -0,0 +1,54 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+
+def main():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("My Snake Game")
+    screen.tracer(0)
+
+    # init objects
+    snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
+
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+
+    isGameOn = True
+    while isGameOn:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
+
+        # Detect collision with food using distance method
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increaseScore()
+
+        # Detect collision with wall.
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+            game_is_on = False
+            scoreboard.gameOver()
+        # Detect collision with tail.
+        for segment in snake.segments:
+            if segment == snake.head:
+                pass
+            elif snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.gameOver()
+
+    screen.exitonclick()
+
+
+if __name__ == '__main__':
+    main()
