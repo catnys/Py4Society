d89b2b1fa043fb82dd51f5b4f706a182bea25ea0
catnys <catnyseverdyn@gmail.com>
2024-07-07 00:46:47 +0300

    Day025: add createuser and writeusers method

--- Day025-Intermediate/TrackProject/main.py
+++ Day025-Intermediate/TrackProject/main.py
@@ -1,25 +1,67 @@
 import csv
+from datetime import datetime
 from user import User
 
-def read_users_from_csv(file_path='data.csv'):
+
+
+def readUsersFromCsv(file_path='data.csv'):
     users = []
     with open(file_path, mode='r') as file:
         csv_reader = csv.DictReader(file, delimiter=';')
         for row in csv_reader:
             name = row['Name']
             total_score = int(row['Total Score'])
-            days = {date: int(score) for date, score in row.items() if date not in ['Name', 'Total Score']}
+            days = {
+                datetime.strptime(date, '%d.%m.%Y'): int(score)
+                for date, score in row.items() if date not in ['Name', 'Total Score']
+            }
             user = User(name, total_score, days)
             users.append(user)
     return users
 
+
+def findUserOnDate(users, name, date_str):
+    date = datetime.strptime(date_str, '%d.%m.%Y')
+    user = findUserByName(users, name)
+    if user:
+        score = user.days.get(date, None)
+        if score is not None:
+            print(f"{user.name}'s score on {date_str}: {score}")
+        else:
+            print(f"No score found for {user.name} on {date_str}")
+    else:
+        print(f"{name} not found")
+
+
+def findUserByName(users, name):
+    for user in users:
+        if user.name == name:
+            return user
+    return None
+
+
+def createUser(name, total_score, days):
+    user = User(name, total_score, days)
+    return user
+
+
+def writeUsersToCsv(users, file_path='data.csv'):
+    with open(file_path, mode='w') as file:
+        csv_writer = csv.writer(file, delimiter=';')
+        for user in users:
+
+
+
 def main():
     """main method"""
     file_path = 'data.csv'  # Ensure this is the correct path to your CSV file
-    users = read_users_from_csv(file_path)
+    users = readUsersFromCsv(file_path)
 
     for user in users:
         print(user)
+
+    findUserOnDate(users, "Dua Lipa", '9.07.2024')
+
 
 if __name__ == "__main__":
     main()
--- Day025-Intermediate/TrackProject/user.py
+++ Day025-Intermediate/TrackProject/user.py
@@ -4,5 +4,17 @@ class User:
         self.total_score = total_score
         self.days = days
 
+    def getName(self):
+        return self.name
+
+    def setName(self, name):
+        self.name = name
+
+    def getTotalScore(self):
+        return self.total_score
+
+    def setTotalScore(self, total_score):
+        self.total_score = total_score
+
     def __repr__(self):
         return f"User(name={self.name}, total_score={self.total_score}, days={self.days})"
