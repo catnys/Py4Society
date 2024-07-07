d94a9fd1e4d1fb014851f65a830efb0f59ea3b5c
catnys <catnyseverdyn@gmail.com>
2024-07-07 18:45:02 +0300

    Day026: init lesson29

---
+++ Day026-Intermediate/Auditorium-Lessons/Lesson29/main.py
@@ -0,0 +1,12 @@
+list_of_strings = input().split(',')
+# 🚨 Do  not change the code above
+
+# TODO: Use list comprehension to convert the strings to integers 👇:
+
+
+# TODO: Use list comprehension to filter out the odd numbers
+# and store the even numbers in a list called "result"
+
+
+# Write your code 👆 above:
+print(result)
