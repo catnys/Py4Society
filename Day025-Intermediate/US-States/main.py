24e0dc6a206b451ba05482050ca2364e58648640
catnys <catnyseverdyn@gmail.com>
2024-07-07 17:15:29 +0300

    Day025: complete day

--- Day025-Intermediate/US-States/main.py
+++ Day025-Intermediate/US-States/main.py
@@ -14,6 +14,14 @@ while len(guessedStates) < 50:
     allStates = data["state"].tolist()
     usrAnswer = screen.textinput(title=f" {len(guessedStates)}/50 are correct", prompt="Which State do you want to guess?").title()
 
+    if usrAnswer.lower() == "exit":
+        missingStates = []
+        for state in allStates:
+            if state not in guessedStates:
+                missingStates.append(state)
+        newData = pandas.DataFrame({"state": missingStates})
+        newData.to_csv("learn_states.csv", index=False)
+        break
     if usrAnswer in allStates:
         guessedStates.append(usrAnswer)
         t = turtle.Turtle()
