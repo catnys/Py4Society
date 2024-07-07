94f69cbd13096e3071e7082eb5b936462a186f62
catnys <catnyseverdyn@gmail.com>
2024-07-07 19:12:23 +0300

    Day026: add lesson30

---
+++ Day026-Intermediate/Auditorium-Lessons/Lesson30/main.py
@@ -0,0 +1,23 @@
+import os
+
+
+def readFile(filename):
+    # Open the file in read mode
+    with open(filename, 'r') as file:
+        # Read the lines of the file and strip newline characters
+        numbers_list = [int(line.strip()) for line in file]
+    return numbers_list
+
+list1 = readFile('file1.txt')
+list2 = readFile('file2.txt')
+
+commonList = [x for x in list2 if x in list1]
+
+# Print the list to verify the content
+print(list1)
+print(list2)
+print("-----")
+print(commonList)
+
+# Write your code above 👆
+# print(result)
