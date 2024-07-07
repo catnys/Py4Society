8abd94ce26bd2ad0fc301d8a58cb0d858eaeb233
catnys <catnyseverdyn@gmail.com>
2024-07-07 16:28:24 +0300

    Day025: init main for US-States

---
+++ Day025-Intermediate/US-States/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.title("U.S. States Game")
+screen.setup(width=800, height=600)
+screen.bgcolor('black')
+screen.tracer(0)
+
+
+
+screen.exitonclick()
