59c9e4d8230e49fb91399e591847ed14e20aa4e1
catnys <catnyseverdyn@gmail.com>
2024-07-07 18:56:50 +0300

    Day026: complete l29

--- Day026-Intermediate/Auditorium-Lessons/Lesson29/main.py
+++ Day026-Intermediate/Auditorium-Lessons/Lesson29/main.py
@@ -1,12 +1,12 @@
-list_of_strings = input().split(',')
+list_of_strings = ['1', '1', '2', '3', '5', '8', '13', '21', '34', '55'] # input().split(',')
 # 🚨 Do  not change the code above
 
 # TODO: Use list comprehension to convert the strings to integers 👇:
-
+list_of_ints = [int(letter) for letter in list_of_strings]
 
 # TODO: Use list comprehension to filter out the odd numbers
 # and store the even numbers in a list called "result"
-
+result = [x for x in list_of_ints if x % 2 == 0]
 
 # Write your code 👆 above:
 print(result)
