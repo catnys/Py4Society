3adf6fd0361e913793d1e50d54f06da0fbaa3fea
catnys <catnyseverdyn@gmail.com>
2024-07-07 21:18:32 +0300

    Day026: init lesson31

--- Day026-Intermediate/Auditorium-Lessons/Lesson31/main.py
+++ Day026-Intermediate/Auditorium-Lessons/Lesson31/main.py
@@ -1,4 +1,21 @@
 sentence = input()
+"""
+What is the Airspeed Velocity of an Unladen Swallow?
+Example Output
+{
+'What': 4, 
+'is': 2, 
+'the': 3, 
+'Airspeed': 8, 
+'Velocity': 8, 
+'of': 2, 
+'an': 2, 
+'Unladen': 7, 
+'Swallow?': 8
+}
+
+"""
+
 # 🚨 Don't change code above 👆
 # Write your code below 👇
 
