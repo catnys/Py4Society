577b5883d77afd38837812616ce3a0dca4151c8b
catnys <catnyseverdyn@gmail.com>
2024-07-07 21:39:45 +0300

    Day026: complete lesson1

--- Day026-Intermediate/Auditorium-Lessons/Lesson31/main.py
+++ Day026-Intermediate/Auditorium-Lessons/Lesson31/main.py
@@ -19,7 +19,14 @@ Example Output
 # 🚨 Don't change code above 👆
 # Write your code below 👇
 
-original_string = "hello world"
-uppercase_string = ''.join([char.upper() for char in original_string])
-print(uppercase_string)
+original_string = "hello world ?"
+stringList = original_string.split(" ")
+myDict = {word: len(word) for word in stringList if word.isalpha()}
+print(myDict)
 
+
+
+#
+# uppercase_string = ''.join([char.upper() for char in original_string])
+# print(uppercase_string)
+#
