9c39416c0730429b25e91f95907883c708bfa20b
catnys <catnyseverdyn@gmail.com>
2024-07-09 22:32:54 +0300

    Day028: start pomodoro

--- Day028-Intermediate/pomodoro-start/main.py
+++ Day028-Intermediate/pomodoro-start/main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +17,17 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50)
+
+canvas = Canvas(width=200, height=224)
+tomato = PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=tomato)
+canvas.create_text(103, 112, text="00:00", fill="white", font=(FONT_NAME,35,"bold"), anchor=CENTER)
+canvas.pack()
+
+
+
+window.mainloop()
+
