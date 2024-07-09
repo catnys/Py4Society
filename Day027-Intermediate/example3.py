483fadb65d22c2626c7a720f0750bdf5876c1631
catnys <catnyseverdyn@gmail.com>
2024-07-09 22:13:11 +0300

    Day027: add example3.py

---
+++ Day027-Intermediate/example3.py
@@ -0,0 +1,6 @@
+def example_function(*args, **kwargs):
+    print("Positional arguments:", args)
+    print("Keyword arguments:", kwargs)
+
+# Calling the function with both positional and keyword arguments
+example_function(1, 2, 3, name='John', age=25)
