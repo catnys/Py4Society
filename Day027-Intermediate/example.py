16590f0b4ad6f0ca70725655b839df9c567ec6b8
catnys <catnyseverdyn@gmail.com>
2024-07-09 22:06:57 +0300

    Day027: add example.py

---
+++ Day027-Intermediate/example.py
@@ -0,0 +1,7 @@
+def example_function(*args):
+    for arg in args:
+        print(arg)
+
+# Calling the function with different numbers of arguments
+example_function(1, 2, 3)
+example_function('a', 'b')
