ff16358220a5438b4011a323e6092236bae2a71e
catnys <catnyseverdyn@gmail.com>
2024-07-09 22:08:11 +0300

    Day027: update example.py

--- Day027-Intermediate/example.py
+++ Day027-Intermediate/example.py
@@ -5,3 +5,9 @@ def example_function(*args):
 # Calling the function with different numbers of arguments
 example_function(1, 2, 3)
 example_function('a', 'b')
+
+
+"""
+- *args is used to pass a variable number of non-keyword arguments to a function. 
+- It allows you to pass any number of positional arguments to your function.
+"""
