b6590e08093b34790c32049809abad0eae579d9f
catnys <catnyseverdyn@gmail.com>
2024-07-09 23:25:16 +0300

    Day029: start password-manager

--- Day029-Intermediate/password-manager/main.py
+++ Day029-Intermediate/password-manager/main.py
@@ -1,5 +1,27 @@
+from tkinter import *
+
+from bokeh.models import canvas
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title('Password Manager')
+window.geometry('500x500')
+window.config(padx=10, pady=10)
+
+canvas = Canvas(width=500, height=500)
+logo = PhotoImage(file='logo.png')
+canvas.create_image(250, 250, image=logo)
+canvas.pack()
+
+
+
+
+window.mainloop()
+
+
+
