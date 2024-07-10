0b789f0a26ae310650f33b585c70ee48c75b10f2
catnys <catnyseverdyn@gmail.com>
2024-07-10 22:30:15 +0300

    Day029: add labels

--- Day029-Intermediate/password-manager/main.py
+++ Day029-Intermediate/password-manager/main.py
@@ -1,27 +1,28 @@
 from tkinter import *
 
-from bokeh.models import canvas
-
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-
-# ---------------------------- UI SETUP ------------------------------- #
-
 window = Tk()
 window.title('Password Manager')
-window.geometry('500x500')
+window.geometry('700x700')
 window.config(padx=10, pady=10)
 
-canvas = Canvas(width=500, height=500)
+# Canvas
+canvas = Canvas(window, width=500, height=500)
 logo = PhotoImage(file='logo.png')
 canvas.create_image(250, 250, image=logo)
-canvas.pack()
+canvas.grid(row=0, column=0, columnspan=2)  # Using grid instead of pack
 
+# Labels
+websiteLabel = Label(window, text='Website')
+websiteLabel.config(font=('Calibri', 20))
+websiteLabel.grid(row=1, column=0)
 
+emailLabel = Label(window, text='Email')
+emailLabel.config(font=('Calibri', 20))
+emailLabel.grid(row=2, column=0)
 
+passwordLabel = Label(window, text='Password')
+passwordLabel.config(font=('Calibri', 20))
+passwordLabel.grid(row=3, column=0)
 
+# Run the Tkinter event loop
 window.mainloop()
-
-
-
