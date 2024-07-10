e0b5afd7aefb0d35ade24655680b18f8fc628176
catnys <catnyseverdyn@gmail.com>
2024-07-10 22:50:50 +0300

    Day029: add generatePasswordButtonClicked

--- Day029-Intermediate/password-manager/main.py
+++ Day029-Intermediate/password-manager/main.py
@@ -1,38 +1,52 @@
 from tkinter import *
 
+
+def generatePasswordButtonClicked():
+    """generatePasswordButtonClicked"""
+    print("generatePasswordButtonClicked")
+
+
 window = Tk()
 window.title('Password Manager')
-window.geometry('700x700')
-window.config(padx=10, pady=10)
+window.geometry('500x500')
+window.config(padx=20, pady=20, bg='#333333')  # Set background color
 
 # Canvas
-canvas = Canvas(window, width=500, height=500)
+canvas = Canvas(window, width=200, height=200, bg='#333333', highlightthickness=0)
 logo = PhotoImage(file='logo.png')
-canvas.create_image(250, 250, image=logo)
-canvas.grid(row=0, column=0, columnspan=2)
+canvas.create_image(150, 150, image=logo)
+canvas.grid(row=0, column=0, columnspan=3, pady=20)  # Adjust padding
 
 # Labels
-websiteLabel = Label(window, text='Website')
+websiteLabel = Label(window, text='Website', fg='white', bg='#333333')
 websiteLabel.config(font=('Calibri', 20))
-websiteLabel.grid(row=1, column=0)
+websiteLabel.grid(row=1, column=0, pady=5, sticky='e')
 
-emailLabel = Label(window, text='Email')
+emailLabel = Label(window, text='Email', fg='white', bg='#333333')
 emailLabel.config(font=('Calibri', 20))
-emailLabel.grid(row=2, column=0)
+emailLabel.grid(row=2, column=0, pady=5, sticky='e')
 
-passwordLabel = Label(window, text='Password')
+passwordLabel = Label(window, text='Password', fg='white', bg='#333333')
 passwordLabel.config(font=('Calibri', 20))
-passwordLabel.grid(row=3, column=0)
+passwordLabel.grid(row=3, column=0, pady=5, sticky='e')
 
 # Entries
 websiteEntry = Entry(window, width=35)
-websiteEntry.grid(row=1, column=1)
+websiteEntry.grid(row=1, column=1, columnspan=2, pady=5, padx=5, sticky='w')
 
 emailEntry = Entry(window, width=35)
-emailEntry.grid(row=2, column=1)
+emailEntry.grid(row=2, column=1, columnspan=2, pady=5, padx=5, sticky='w')
 
-passwordEntry = Entry(window, width=35)
-passwordEntry.grid(row=3, column=1)
+passwordEntry = Entry(window, width=21)
+passwordEntry.grid(row=3, column=1, pady=5, padx=5, sticky='w')
+
+# Generate Password Button
+generatePasswordButton = Button(window, text='Generate Password', command=generatePasswordButtonClicked)
+generatePasswordButton.grid(row=3, column=2, pady=5, padx=5, sticky='w')
+
+# Save Button
+saveButton = Button(window, text='Save', width=36, command=saveButtonClicked)
+saveButton.grid(row=4, column=1, columnspan=2, pady=10)
 
 # Run the Tkinter event loop
 window.mainloop()
