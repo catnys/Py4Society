0961dd7ffe8dadff2cd1a0725e6e0576fc1d4489
catnys <catnyseverdyn@gmail.com>
2024-07-15 22:25:21 +0300

    Add createMessage method

--- Day032-IntermediatePlus/MotivationReminder/main.py
+++ Day032-IntermediatePlus/MotivationReminder/main.py
@@ -2,3 +2,13 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+
+def createMessage(sender, receiver, subject, body):
+    # Create the email content
+    message = MIMEMultipart()
+    message["From"] = sender
+    message["To"] = receiver
+    message["Subject"] = subject
+
+    # Attach the body text
+    message.attach(MIMEText(body, "plain"))
