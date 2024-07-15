b813f46551a782528a16db1b1262878e19eedbef
catnys <catnyseverdyn@gmail.com>
2024-07-15 22:13:42 +0300

    Add getters and setters

--- Day032-IntermediatePlus/MotivationReminder/email.py
+++ Day032-IntermediatePlus/MotivationReminder/email.py
@@ -16,3 +16,28 @@ class Email:
 
     def getPassword(self):
         return self.password
+
+    def setSender(self,sender):
+        self.sender=sender
+
+    def getSender(self):
+        return self.sender
+
+    def setReceiver(self,receiver):
+        self.receiver=receiver
+
+    def getReceiver(self):
+        return self.receiver
+
+    def setSubject(self,subject):
+        self.subject=subject
+
+    def getSubject(self):
+        return self.subject
+
+    def setBody(self,body):
+        self.body=body
+
+    def getBody(self):
+        return self.body
+
