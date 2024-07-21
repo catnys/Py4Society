af533763805c87486e965c661f161cdfe5a236c0
catnys <catnyseverdyn@gmail.com>
2024-07-21 12:59:58 -0700

    Day035: update main.py

--- Day035-IntermediatePlus/RainAlert/main.py
+++ Day035-IntermediatePlus/RainAlert/main.py
@@ -1,2 +1,13 @@
-API_KEY = '643475f9e34c11070abdf7e99273caaf'
+import requests
 
+OWM_ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast'
+API_KEY = '<API KEY>'
+
+weatherParams = {
+    'lat': 34.052235,
+    'long': -118.243683,
+    'appid': API_KEY
+}
+
+response = requests.get(OWM_ENDPOINT, params=weatherParams)
+print(response.json())
