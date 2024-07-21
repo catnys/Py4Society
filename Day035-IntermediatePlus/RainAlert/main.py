0903f1dc0c445043f42d34f4aec27ecdbf076094
catnys <catnyseverdyn@gmail.com>
2024-07-21 14:07:59 -0700

    Day035: update main

--- Day035-IntermediatePlus/RainAlert/main.py
+++ Day035-IntermediatePlus/RainAlert/main.py
@@ -4,10 +4,19 @@ OWM_ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast'
 API_KEY = '<YOUR_API_KEY>'
 
 weatherParams = {
-    'lat': 34.052235,
-    'lon': -118.243683,
+    'lat': 45.657974,
+    'lon': 25.601198,
+    'cnt': 4,
     'appid': API_KEY
 }
-
+isRain = False
 response = requests.get(OWM_ENDPOINT, params=weatherParams)
-print(response.json())
+# print(response.json()['list'][0]['weather'][0]['id'])
+
+for hour in response.json()['list']:
+    wcode = hour['weather'][0]['id']
+    isRain = True if int(wcode) < 700 else False
+
+if isRain:
+    print(f"bring an umbrella.")
+
