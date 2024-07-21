a046c1d6f122bddd60f15f801a48c94d834edf09
catnys <catnyseverdyn@gmail.com>
2024-07-20 17:06:14 -0700

    Day034: update gui

--- Day034-IntermediatePlus/Quizzler/ui.py
+++ Day034-IntermediatePlus/Quizzler/ui.py
@@ -14,7 +14,15 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
         self.questionText = self.canvas.create_text(150, 125, text="Some quest text", fill=THEME_COLOR, font="Arial 20 italic")
-        self.canvas.grid(row=1, column=0,columnspan=2)
+        self.canvas.grid(row=1, column=0,columnspan=2, pady=50)
+        trueImg = PhotoImage(file="images/true.png")
+        self.trueButton = Button(image=trueImg, highlightthickness=0)
+        self.trueButton.grid(row=2, column=0)
+
+        falseImg = PhotoImage(file="images/false.png")
+        self.falseButton = Button(image=falseImg, highlightthickness=0)
+        self.falseButton.grid(row=2, column=1)
+
 
 
 
