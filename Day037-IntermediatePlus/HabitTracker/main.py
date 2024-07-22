47d4c66187669651083622db4bc80d2d8fc82979
catnys <catnyseverdyn@gmail.com>
2024-07-22 09:36:31 +0300

    Day037: add pixel

--- Day037-IntermediatePlus/HabitTracker/main.py
+++ Day037-IntermediatePlus/HabitTracker/main.py
@@ -4,6 +4,7 @@ PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 PIXELA_API_KEY = "<KEY>"
 TOKEN = "<TOKEN>"
 USERNAME = "katniss"
+GRAPH_ID = "<GRAPH_ID>"
 
 
 PIXELA_GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
@@ -21,7 +22,7 @@ Parameters = {
 
 
 graphConfig = {
-    "id": "<graph-id>",
+    "id": GRAPH_ID,
     "name": "Sports Graph",
     "unit": "Km",
     "type": "float",
@@ -31,7 +32,18 @@ graphConfig = {
 headers = {
     "X-USER-TOKEN": TOKEN
 }
-
+"""
 # {'message': 'Success.', 'isSuccess': True}
 response = requests.post(PIXELA_GRAPH_ENDPOINT, json=graphConfig, headers=headers)
+print(response.json())
+"""
+
+PIXELA_CREATE_PIXEL_ENDPOINT = f"{PIXELA_GRAPH_ENDPOINT}/{GRAPH_ID}"
+
+pixelData = {
+    "date": "20240402",
+    "quantity": "9.75"
+}
+
+response = requests.post(url=PIXELA_CREATE_PIXEL_ENDPOINT,json=pixelData, headers=headers)
 print(response.json())
