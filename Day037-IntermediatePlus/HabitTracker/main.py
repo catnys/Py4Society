fd9f44e2035c493c9a062072ab466d332be12e9c
catnys <catnyseverdyn@gmail.com>
2024-07-22 09:26:11 +0300

    Day037: add graph

--- Day037-IntermediatePlus/HabitTracker/main.py
+++ Day037-IntermediatePlus/HabitTracker/main.py
@@ -2,13 +2,36 @@ import requests
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 PIXELA_API_KEY = "<KEY>"
+TOKEN = "<TOKEN>"
+USERNAME = "katniss"
+
+
+PIXELA_GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
 
 Parameters = {
-    "token": "<YOUR TOKEN HERE>",
-    "username": "katniss",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
-    "notMinor": "yes"
+    "notMinor": "no"
 }
 
-response = requests.post(url=PIXELA_ENDPOINT, json=Parameters)
+# response = requests.post(url=PIXELA_ENDPOINT, json=Parameters)
+# print(response.json())
+
+
+graphConfig = {
+    "id": "<graph-id>",
+    "name": "Sports Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# {'message': 'Success.', 'isSuccess': True}
+response = requests.post(PIXELA_GRAPH_ENDPOINT, json=graphConfig, headers=headers)
 print(response.json())
