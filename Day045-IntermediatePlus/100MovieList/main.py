20bf32580457bd63e3ce9b9c98afb4683169838f
catnys <catnyseverdyn@gmail.com>
2024-07-24 23:05:46 +0300

    Day045: complete day 45 project

--- Day045-IntermediatePlus/100MovieList/main.py
+++ Day045-IntermediatePlus/100MovieList/main.py
@@ -5,4 +5,20 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, "html.parser")
+# print(soup.prettify())
+
+movieNames = []
+# Adjusted to find the correct <h3> element with class "title"
+for article in soup.find_all(name="h3", attrs={"class": "title"}):
+    print(article.text.strip())
+    movieNames.append(article.text.strip())
+
+
+with open("movies.txt", "w") as file:
+    for i in range(len(movieNames)-1, -1, -1):
+        file.write(movieNames[i] + "\n")
+
+
 
