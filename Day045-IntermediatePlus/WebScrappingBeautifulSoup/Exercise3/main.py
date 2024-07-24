20d9ea45280d57c18c860a6f407fdcad29c6cf35
catnys <catnyseverdyn@gmail.com>
2024-07-24 22:03:07 +0300

    Day045: init main.py

--- Day045-IntermediatePlus/WebScrappingBeautifulSoup/Exercise3/main.py
+++ Day045-IntermediatePlus/WebScrappingBeautifulSoup/Exercise3/main.py
@@ -1,1 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
 
+response = requests.get('https://appbrewery.github.io/news.ycombinator.com')
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# print(soup.prettify())
