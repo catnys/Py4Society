0ccc35cb9e45f801fffa05790c4c51303ebf8bb8
catnys <catnyseverdyn@gmail.com>
2024-07-24 22:09:38 +0300

    Day045: ... so on

--- Day045-IntermediatePlus/WebScrappingBeautifulSoup/Exercise3/main.py
+++ Day045-IntermediatePlus/WebScrappingBeautifulSoup/Exercise3/main.py
@@ -4,4 +4,7 @@ import requests
 response = requests.get('https://appbrewery.github.io/news.ycombinator.com')
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# print(soup.prettify())
+articles = soup.find_all(name='a', class_='storylink')
+for article in articles:
+    print(article.text)
+
