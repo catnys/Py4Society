e914286a8470899bf656106c60322e33089f5f1e
catnys <catnyseverdyn@gmail.com>
2024-07-25 12:58:04 +0300

    Day056: add static

--- Day056-IntermediatePlus/PersonalCard/server.py
+++ Day056-IntermediatePlus/PersonalCard/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("CurriculumVitae.html")
+    return render_template("curriculumvitae.html")
 
 
 if __name__ == '__main__':
