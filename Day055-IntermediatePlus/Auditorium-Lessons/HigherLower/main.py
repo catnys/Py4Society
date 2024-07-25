691aa51198e7303e1eabf9177758bfe07b7b85f4
catnys <catnyseverdyn@gmail.com>
2024-07-25 12:31:13 +0300

    Day055: update main

--- Day055-IntermediatePlus/Auditorium-Lessons/HigherLower/main.py
+++ Day055-IntermediatePlus/Auditorium-Lessons/HigherLower/main.py
@@ -8,6 +8,16 @@ def hello_world():
     return 'Hello, World!'
 
 
+@app.route('/bye')
+def bye():
+    return 'byee!'
+
+
+@app.route('/<name>')
+def greeting(name):
+    return f"Hello, there! , {name}"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
