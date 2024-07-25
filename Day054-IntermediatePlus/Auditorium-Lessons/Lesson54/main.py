e0cf5e2bbab6fc76453881ab9a75e8761d4d82cd
catnys <catnyseverdyn@gmail.com>
2024-07-25 11:39:48 +0300

    Day054: add solution to Auditorium

--- Day054-IntermediatePlus/Auditorium-Lessons/Lesson54/main.py
+++ Day054-IntermediatePlus/Auditorium-Lessons/Lesson54/main.py
@@ -1,20 +1,24 @@
 import time
-
 current_time = time.time()
-print(current_time)  # seconds since Jan 1st, 1970
+print(current_time)
 
+def speed_calc_decorator(function):
+  def wrapper_function():
+    start_time = time.time()
+    function()
+    end_time = time.time()
+    print(f"{function.__name__} run speed: {end_time - start_time}s")
+  return wrapper_function
 
-# Write your code below 👇
-
-def speed_calc_decorator():
-    pass
-
-
+@speed_calc_decorator
 def fast_function():
-    for i in range(1000000):
-        i * i
+  for i in range(1000000):
+    i * i
 
-
+@speed_calc_decorator
 def slow_function():
-    for i in range(10000000):
-        i * i
+  for i in range(10000000):
+    i * i
+
+fast_function()
+slow_function()
