47b4b5b57d86e4411ef76c21e64b2aab05c97199
catnys <catnyseverdyn@gmail.com>
2024-07-26 15:35:03 +0300

    Day084: update main

--- Day084-Advanced/TicTacToe/main.py
+++ Day084-Advanced/TicTacToe/main.py
@@ -14,24 +14,21 @@ from player import Player
 
 boardArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 
-# Set Walls - Wunderbar :)
-def setRP(isSpace):
+
+def displayBoard():
+    # init vars
     horizontal = "-------"
     vertical = "|"
     space = "       "
+    isSpace = False
 
-    for i in range(3):
-        print(space, end="") if isSpace else print(horizontal, end="")
-        if i != 2: print(vertical, end="")
-    print()  # Move to the next line after printing the row
-
-
-def printBoard():
-    isSpace = False  # Initial state
 
     for row in range(7):
-        setRP(isSpace)
+        for i in range(3):
+            print(space, end="") if isSpace else print(horizontal, end="")
+            if i != 2: print(vertical, end="")
+        print()  # Move to the next line after printing the row
         isSpace = not isSpace  # Toggle the value of isSpace at the end of each row iteration
 
 
-printBoard()
+displayBoard()
